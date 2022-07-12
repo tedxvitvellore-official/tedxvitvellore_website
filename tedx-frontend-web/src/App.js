6824afd1205cb8d243d1d8332d8ea6c088f9924c
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-07-13 00:04:24 +0530

    boiler code in all components

--- tedx-frontend-web/src/App.js
+++ tedx-frontend-web/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Navbar />}>
+    //       <Route index element={<HeroSection />} />
+    //       <Route path="contact" element={<Contact />} />
+    //       <Route path="*" element={<NoPage />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <>
+    {/* render what you want to render */}
+    <Navbar/>
+
+    </>
   );
 }
 
