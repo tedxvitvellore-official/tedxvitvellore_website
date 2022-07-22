0f8e72c9b77d542c4bd8c57161e7de04e4ccfec0
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-07-22 23:37:17 +0530

    date layout changed

--- tedx-frontend-web/src/components/Event/Event.jsx
+++ tedx-frontend-web/src/components/Event/Event.jsx
@@ -5,9 +5,17 @@ function Event({ imgURL, desc, dates, classStyle }) {
     <div className="flex flex-col md:flex-row items-center justify-around w-3/4 text-white mt-20">
       <div className="flex flex-col items-center justify-around w-1/2">
         <img src={imgURL} alt="imgURL" className={classStyle}/>
-        <div className="text-pm py-8 text-3xl text-center md:text-left">{dates}</div>
       </div>
-      <div className="pl-4 max-w-lg text-center md:text-left">{desc}</div>
+      <div className="pl-4 flex flex-col">
+      <div className=" max-w-lg text-center md:text-left">{desc}</div>
+      <div className="flex items-center ">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+      </svg>
+        <div className="text-pm py-8 text-xl text-center md:text-left px-2">{dates}</div>  
+      </div>
+      </div>
+      
     </div>
   );
 }
